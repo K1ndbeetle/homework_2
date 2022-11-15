02560e0e3909819360a8a281455303ff17cac405
K1ndbeetle <alx-s@bk.ru>
2022-11-15 22:57:38 +0300

    Task №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task №1
+        System.out.println("Задача №1");
+        int oranges = 1234;
+        byte lemons = 123;
+        short apples = 12345;
+        long pears = 12345678910L;
+        float pineapples = 1.2f;
+        double kiwi = 1.234;
+        System.out.println("Зачение переменной oranges с типом int равно " + oranges);
+        System.out.println("Зачение переменной lemons с типом byte равно " +lemons);
+        System.out.println("Зачение переменной apples с типом short равно " + apples);
+        System.out.println("Зачение переменной pears с типом long равно " + pears);
+        System.out.println("Зачение переменной pineapples с типом float равно " + pineapples);
+        System.out.println("Зачение переменной kiwi с типом double равно " + kiwi );
     }
 }
